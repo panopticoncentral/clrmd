7a5f9f38dd5f8a5c80a86be6c4484d6f4405d383
Paul Vick <paulv@microsoft.com>
2020-11-05 15:04:09 -0800

    Fix format error

--- src/Microsoft.Diagnostics.Runtime/src/Implementation/ClrmdStringType.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Implementation/ClrmdStringType.cs
@@ -181,8 +181,8 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
         // TODO: remove
         public override ClrInstanceField? GetFieldByName(string name) => Fields.FirstOrDefault(f => f.Name == name);
 
-
         private const uint FinalizationSuppressedFlag = 0x40000000;
+
         public override bool IsFinalizeSuppressed(ulong obj)
         {
             // TODO move to ClrObject?
